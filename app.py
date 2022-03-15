7b8cef6374724b65043dff70878ced24aa942f6d
Ethan Tao <eth@ntao.ca>
2022-03-14 22:29:33 -0400

    add menu items to the database

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from sqlalchemy.ext.automap import automap_base
 app = Flask(__name__)
 
 # replace the user name and password in the statement below
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/bluescafe'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/bluescafe'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ethan:w8Q1Ji8I23s2r4YIsocemabAb5nEQo@192.168.1.15/bluescafe'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -28,6 +29,22 @@ def index():
     menus = db.session.query(Page.name).order_by(Page.order.asc())
     pages = db.session.query(Page.content).order_by(Page.order.asc())
     return render_template('index.html', pages=pages, menus=menus, foodMenu=foodMenu)
+
+
+@ app.route('/addmenuitem', methods=["GET", "POST"])
+def addMenuItem():
+    if request.method == "POST":
+        items = request.form.getlist('item')
+        dates = request.form.getlist('date')
+
+        for i in range(len(items)):
+            new_item = Menu(item=items[i], day=dates[i])
+            db.session.add(new_item)
+        db.session.commit()
+
+        return ""
+    else:
+        return render_template("addmenuitem.html")
 
 
 if __name__ == '__main__':
